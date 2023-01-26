373ec1bc4b209e54f976238cfdc784ec4da4760c
Kris Kasprzak <kris.kasprzak@yahoo.com>
2023-01-26 14:46:07 -0600

    Update WebPageUpdate.ino

--- WebPageUpdate.ino
+++ WebPageUpdate.ino
@@ -30,6 +30,27 @@
 
   NOTE if your ESP fails to program press the BOOT button during programm when the IDE is "looking for the ESP"
 
+  The MIT License (MIT)
+
+  code writen by Kris Kasprzak
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy of
+  this software and associated documentation files (the "Software"), to deal in
+  the Software without restriction, including without limitation the rights to
+  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+  the Software, and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+  On a personal note, if you develop an application or product using this code 
+  and make millions of dollars, I'm happy for you!
 
 */
 
